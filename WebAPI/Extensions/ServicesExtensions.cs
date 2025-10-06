93bf33e0ee8fa4ad882aa0d70377a11504fa2acd
Ibrahim Parlak <ibrahimparlak3694@gmail.com>
2025-10-06 22:16:09 +0300

    Limit Sınırlama

--- WebAPI/Extensions/ServicesExtensions.cs
+++ WebAPI/Extensions/ServicesExtensions.cs
@@ -10,6 +10,7 @@ using Services.Contracts;
 using Microsoft.AspNetCore.Mvc.Versioning;
 using Presentation.Controller;
 using Marvin.Cache.Headers;
+using AspNetCoreRateLimit;
 
 namespace WebAPI.Extensions
 {
@@ -124,7 +125,30 @@ namespace WebAPI.Extensions
                 validationOpt.MustRevalidate = false;
             });
         }
-            
+
+        public static void ConfigureRateLimitingOptions(this IServiceCollection services)
+        {
+            var rateLimitRules = new List<RateLimitRule>()
+            {
+                new RateLimitRule()
+                {
+                    Endpoint = "*",
+                    Limit = 3,
+                    Period = "1m"
+                }
+            };
+
+            services.Configure<IpRateLimitOptions>(opt =>
+            {
+                opt.GeneralRules = rateLimitRules;
+            });
+
+            services.AddSingleton<IRateLimitCounterStore, MemoryCacheRateLimitCounterStore>();
+            services.AddSingleton<IIpPolicyStore, MemoryCacheIpPolicyStore>();
+            services.AddSingleton<IRateLimitConfiguration, RateLimitConfiguration>();
+            services.AddSingleton<IProcessingStrategy, AsyncKeyLockProcessingStrategy>();
+        }
+
 
     }
 }
